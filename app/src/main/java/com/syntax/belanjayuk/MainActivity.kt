74b4902e1a42de694b93b81f21b146e7f8d3b612
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-05-10 06:05:12 +0700

    update dulu

--- app/src/main/java/com/syntax/belanjayuk/MainActivity.kt
+++ app/src/main/java/com/syntax/belanjayuk/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.syntax.belanjayuk
 
-import android.app.Activity
 import android.os.Bundle
 
 import android.support.v4.view.GravityCompat
---
+++ app/src/main/java/com/syntax/belanjayuk/utils/FormatHelpers.kt
@@ -0,0 +1,2 @@
+package com.syntax.belanjayuk.utils
+
